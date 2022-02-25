dd5f93b5223390dc814d257b116891019a86a4de
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2022-02-25 10:41:52 +0300

    uploading the new recipe app updata. The app is not finish yet but almost only js file is not complet yet

---
+++ Recipe App/script.js
@@ -0,0 +1,29 @@
+const meals = document.getElementById('meals');
+
+getRandomMeal()
+
+async function getRandomMeal()
+{
+    const resp = await (await fetch('https://www.themealdb.com/api/json/v1/1/random.php'));
+    const respData = await resp.json();
+    const randomMeal = respData.meals[0]
+    console.log(randomMeal);
+
+    addMeal(randomMeal, true);
+}
+
+async function getMealById(id)
+{
+    const meal = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
+}
+
+async function getMealBySearch(term)
+{
+    const meals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + term);
+}
+
+// function
+addMeal(mealData, random = false)
+{
+    
+}

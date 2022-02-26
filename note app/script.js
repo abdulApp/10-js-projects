34298fced73bdbcb5e62c8ee443897672e10f1ae
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2022-02-26 11:57:26 +0300

    fixing and creating now vairables in js file

--- note app/script.js
+++ note app/script.js
@@ -1,17 +1,49 @@
-const noteEl = document.querySelector('.notes');
-const editBtn = document.querySelector('.edit');
-const deleteBtn = document.querySelector('.delete');
+const addBtn = document.getElementById('add');
 
-const main = noteEl.querySelector('.main');
-const textArea = noteEl.querySelector('textarea');
 
-editBtn.addEventListener('click', () => {
-    main.classList.toggle('hidden');
-    textArea.classList.toggle('hidden');
+addBtn.addEventListener('click', () => 
+{
+    addNewNote();
 });
 
-textArea.addEventListener('input', (e) => {
-    const { value } = e.target;
+function addNewNote()
+{
+    const note = document.createElement('div');
+    note.classList.add('note');
 
-    main.innerHTML = marked(value);
-});
+    note.innerHTML = `
+    <div class="notes">
+        <div class="tools">
+            <button class="edit"><i class="fas fa-edit"></i></button>
+            <button class="delete"><i class="fas fa-trash-alt"></i></button>
+        </div>
+        <div class="main hidden">
+        </div>
+        <textarea></textarea>
+    </div>    
+    `;
+
+    const editBtn = note.querySelector('.edit');
+    const deleteBtn = note.querySelector('.delete');
+
+    const main = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+
+    editBtn.addEventListener('click', () => {
+        main.classList.toggle('hidden');
+        textArea.classList.toggle('hidden');
+    });
+
+    deleteBtn.addEventListener('click', () => 
+    {
+        note.remove();
+    });
+
+    textArea.addEventListener('input', (e) => {
+        const { value } = e.target;
+
+        main.innerHTML = marked(value);
+    });
+
+    document.body.appendChild(note);
+}

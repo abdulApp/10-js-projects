b369d1971aaf16b89c4752aa703d35cd94dff6ba
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2022-02-26 11:31:34 +0300

    adding now button

--- note app/script.js
+++ note app/script.js
@@ -8,4 +8,10 @@ const textArea = noteEl.querySelector('textarea');
 editBtn.addEventListener('click', () => {
     main.classList.toggle('hidden');
     textArea.classList.toggle('hidden');
-})
+});
+
+textArea.addEventListener('input', (e) => {
+    const { value } = e.target;
+
+    main.innerHTML = marked(value);
+});

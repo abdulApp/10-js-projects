01226818ed7a706838a3e218ac5dac67e78548f0
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2022-02-26 15:39:14 +0300

    the app is able now to: add, mark, remove or delete

---
+++ todo app/script.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todos = document.getElementById('todos');
+
+form.addEventListener('submit', (e) =>
+    {
+        e.preventDefault();
+        const todoText = input.value;
+
+        if(todoText)
+        {
+            const todoEl = document.createElement('li');
+            todoEl.innerHTML = todoText;
+
+            todoEl.addEventListener('click', () =>
+                {
+                    todoEl.classList.toggle('completed');
+                }
+            );
+
+            todoEl.addEventListener('contextmenu', (e) =>
+                {
+                    e.preventDefault();
+
+                    todoEl.remove();
+                }
+            );
+
+            todos.appendChild(todoEl);
+
+            input.value = "";
+        }
+    }
+);
